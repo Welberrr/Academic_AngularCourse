1499a37e1d99ea7760264e364ac4965506ea2ef8
Welberrr <welberhenrique.costa@gmail.com>
2025-09-30 21:30:55 -0300

    Feat: Finalizacao de bindings (Two-Way, Class e Style bindings

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   tituloHeader = 'Youtube Welber Henrique';
+  valorInput = 'Valor Inicial'
+  aplicarClasse = false;
+  estiloCor = 'orange';
 
   retornarTitulo(){
     return 'Welber';
   }
 
   atualizarTitulo(){
-    this.tituloHeader = 'Welber atualizado';
+    this.estiloCor = 'red';
+    this.aplicarClasse = !this.aplicarClasse;
+    console.log(this.valorInput)
   }
 }
