e2923be23fc77d6f8759c74ce139c756db72c5b1
Welberrr <welberhenrique.costa@gmail.com>
2025-10-03 14:47:11 -0300

    Feat: Requisicoe Http

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { StorageService } from '../../services/storage.service';
+import { IPost, JsonPlaceholderService } from '../../services/json-placeholder.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-content',
@@ -10,5 +12,15 @@ import { StorageService } from '../../services/storage.service';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-  readonly _storageService = inject(StorageService);
+postsList: IPost[]= [];
+
+ readonly _jsonPlaceholderService = inject(JsonPlaceholderService);
+
+ ngOnInit(){
+  this._jsonPlaceholderService.getPosts().subscribe(
+    (response)=> {console.log('response: ', response);
+    this.postsList = response;
+    }
+  )
+ }
 }
