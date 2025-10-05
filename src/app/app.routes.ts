93e5108c2c5b2c2d510bee32304d9ce5cd788375
Welberrr <welberhenrique.costa@gmail.com>
2025-10-04 21:24:40 -0300

    Feat: Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: 'header', component: HeaderComponent},
+{ path: 'content', component: ContentComponent},
+];
