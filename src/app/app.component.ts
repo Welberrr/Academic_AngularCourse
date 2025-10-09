6eee767c3c071ea678fe345f13893327cb9d4451
Welberrr <welberhenrique.costa@gmail.com>
2025-10-09 17:26:09 -0300

    Feat: Components com CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './helloWorld/helloWorld.component'
+import { MinhaPaginaComponent } from './minha-pagina/minha-pagina.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [HelloWorldComponent, MinhaPaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
