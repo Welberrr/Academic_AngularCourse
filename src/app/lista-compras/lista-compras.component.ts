721431a90d9ff7c5adc9934cc1c2bd460f19eb07
Welberrr <welberhenrique.costa@gmail.com>
2025-10-10 14:24:59 -0300

    Feat: Adicao de itens no formulario

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
   
+  item: string = '';
+
+  adicionar(){
+    console.log("Item adicionado: " + this.item);
+    this.item = '';
+  }
 }
