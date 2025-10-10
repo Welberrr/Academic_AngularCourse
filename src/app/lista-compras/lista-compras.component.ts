01c8a683aecf399d931b3b20391c75aadfa68c60
Welberrr <welberhenrique.costa@gmail.com>
2025-10-10 14:34:58 -0300

    Feat: Classe de modelo de Arrays

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemLista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -10,9 +11,17 @@ import { FormsModule } from '@angular/forms';
 export class ListaComprasComponent {
   
   item: string = '';
+  lista: ItemLista[] = [];
 
   adicionar(){
-    console.log("Item adicionado: " + this.item);
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista);
   }
 }
